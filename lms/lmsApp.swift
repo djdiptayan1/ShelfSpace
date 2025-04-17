80a1480eb63664418cf604851d1ce8b44536cb4b
Diptayan Jash <djdiptayan1@gmail.com>
2025-04-17 23:23:28 +0530

    initial ✨✨

---
+++ lms/lmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  lmsApp.swift
+//  lms
+//
+//  Created by Diptayan Jash on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct lmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
