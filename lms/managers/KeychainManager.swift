34bfff0b549a8a4490426a805e5ae8e386ec6508
Diptayan Jash <djdiptayan1@gmail.com>
2025-04-22 18:15:35 +0530

    feat: implement KeychainManager for secure token storage and retrieval

---
+++ lms/managers/KeychainManager.swift
@@ -0,0 +1,87 @@
+import Foundation
+import Security
+
+enum KeychainError: Error {
+    case duplicateEntry
+    case unknown(OSStatus)
+    case noToken
+    case unexpectedTokenData
+}
+
+class KeychainManager {
+    static let shared = KeychainManager()
+    
+    private init() {}
+    
+    private let service = "com.lms.app"
+    private let account = "jwtToken"
+    
+    func saveToken(_ token: String) throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecValueData as String: token.data(using: .utf8)!
+        ]
+        
+        let status = SecItemAdd(query as CFDictionary, nil)
+        
+        if status == errSecDuplicateItem {
+            try updateToken(token)
+        } else if status != errSecSuccess {
+            throw KeychainError.unknown(status)
+        }
+    }
+    
+    func getToken() throws -> String {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true
+        ]
+        
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        
+        guard status == errSecSuccess,
+              let data = result as? Data,
+              let token = String(data: data, encoding: .utf8) else {
+            throw KeychainError.noToken
+        }
+        
+        return token
+    }
+    
+    private func updateToken(_ token: String) throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        
+        let attributes: [String: Any] = [
+            kSecValueData as String: token.data(using: .utf8)!
+        ]
+        
+        let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+        
+        if status != errSecSuccess {
+            throw KeychainError.unknown(status)
+        }
+    }
+    
+    func deleteToken() throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account
+        ]
+        
+        let status = SecItemDelete(query as CFDictionary)
+        
+        if status != errSecSuccess && status != errSecItemNotFound {
+            throw KeychainError.unknown(status)
+        }
+    }
+} 
