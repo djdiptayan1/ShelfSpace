c16080bedc6a0fcc693353b213d1aa7b01471b16
Diptayan Jash <djdiptayan1@gmail.com>
2025-04-22 18:18:09 +0530

    feat: implement UserTabbar with navigation for Home and Collections

---
+++ lms/view_controllers/tabbar/userTabbar.swift
@@ -0,0 +1,76 @@
+//
+//  librarianTabbar.swift
+//  lms
+//
+//  Created by Diptayan Jash on 18/04/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct UserTabbar: View {
+    @State private var selectedTab = 0
+    @Environment(\.colorScheme) private var colorScheme
+    @StateObject private var themeManager = ThemeManager()
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            HomeViewUser()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+
+            BookCollectionuser()
+                .tabItem {
+                    Image(systemName: "books.vertical.fill")
+                    Text("Collections")
+                }
+        }
+        .accentColor(Color.primary(for: colorScheme))
+        .onAppear {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = UIColor(Color.TabbarBackground(for: colorScheme))
+            
+            let itemAppearance = UITabBarItemAppearance()
+            itemAppearance.normal.iconColor = UIColor(Color.text(for: colorScheme).opacity(0.6))
+            itemAppearance.normal.titleTextAttributes = [
+                .foregroundColor: UIColor(Color.text(for: colorScheme).opacity(0.6))
+            ]
+            itemAppearance.selected.iconColor = UIColor(Color.primary(for: colorScheme))
+            itemAppearance.selected.titleTextAttributes = [
+                .foregroundColor: UIColor(Color.primary(for: colorScheme))
+            ]
+            
+            appearance.stackedLayoutAppearance = itemAppearance
+            appearance.inlineLayoutAppearance = itemAppearance
+            appearance.compactInlineLayoutAppearance = itemAppearance
+            
+            UITabBar.appearance().standardAppearance = appearance
+            if #available(iOS 15.0, *) {
+                UITabBar.appearance().scrollEdgeAppearance = appearance
+            }
+        }
+    }
+}
+
+struct HomeViewUser: View {
+    var body: some View {
+        NavigationView {
+            Text("Home Screen user")
+                .navigationTitle("Home")
+        }
+    }
+}
+
+struct BookCollectionuser: View {
+    var body: some View {
+        NavigationView {
+            Text("Books collection Screen user")
+                .navigationTitle("Books")
+        }
+    }
+}
+#Preview {
+    UserTabbar()
+}
