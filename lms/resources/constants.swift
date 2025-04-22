cad8fdae67f2669eab5c8a54ef607ff8696406b8
Diptayan Jash <djdiptayan1@gmail.com>
2025-04-22 18:16:49 +0530

    feat: update ColorConstants with additional light and dark background colors

--- lms/resources/constants.swift
+++ lms/resources/constants.swift
@@ -11,13 +11,18 @@ enum ColorConstants {
     // Light Theme Colors
     static let lightText = "#212121"
     static let lightBackground = "#fafafa"
+    static let lightBackground1 = "#80d3fa"
+    
     static let lightPrimary = "#0097a8"
     static let lightSecondary = "#2c7be2"
     static let lightAccent = "#80d3fa"
     
     // Dark Theme Colors
     static let darkText = "#dedede"
+//    static let darkText = "#5eeb34"
     static let darkBackground = "#050505"
+    static let darkBackground1 = "#05597f"
+    
     static let darkPrimary = "#57eeff"
     static let darkSecondary = "#1d6cd3"
     static let darkAccent = "#05597f"
