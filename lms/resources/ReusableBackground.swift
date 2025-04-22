ae1f4f47f9479b56159092c01b018e5f63f171b4
Diptayan Jash <djdiptayan1@gmail.com>
2025-04-22 18:17:00 +0530

    feat: add ReusableBackground component for gradient backgrounds

---
+++ lms/resources/ReusableBackground.swift
@@ -0,0 +1,25 @@
+//
+//  ReusableBackground.swift
+//  lms
+//
+//  Created by Diptayan Jash on 18/04/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct ReusableBackground: View {
+    let colorScheme: ColorScheme
+
+    var body: some View {
+        LinearGradient(
+            gradient: Gradient(colors: [
+                Color.background(for: colorScheme).opacity(0.3),
+                Color.background(for: colorScheme).opacity(0.1)
+            ]),
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+        .ignoresSafeArea()
+    }
+}
