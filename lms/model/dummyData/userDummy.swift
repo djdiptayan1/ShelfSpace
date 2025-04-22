067478f2735e7147e74724d05ad0222eacd4378c
Diptayan Jash <djdiptayan1@gmail.com>
2025-04-22 18:16:37 +0530

    feat: add userDummy data file for user model testing

---
+++ lms/model/dummyData/userDummy.swift
@@ -0,0 +1,34 @@
+//
+//  userDummy.swift
+//  lms
+//
+//  Created by Diptayan Jash on 18/04/25.
+//
+
+import Foundation
+
+//class UserDummy {
+//    static let users: [User] = [
+//        User(
+//            id: "1",
+//            email: "admin@library.com",
+//            password: "admin123",
+//            role: .admin,
+//            name: "Admin User"
+//        ),
+//        User(
+//            id: "2",
+//            email: "librarian@library.com",
+//            password: "librarian123",
+//            role: .librarian,
+//            name: "Librarian User"
+//        ),
+//        User(
+//            id: "3",
+//            email: "user@library.com",
+//            password: "user123",
+//            role: .user,
+//            name: "Regular User"
+//        )
+//    ]
+//}
