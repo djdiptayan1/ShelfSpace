6c9058e1e69c2733ae8d2a21c0bdc5cc6cd1e2fa
Diptayan Jash <djdiptayan1@gmail.com>
2025-05-09 03:51:35 +0530

    refactor: Replace hardcoded API key and base URL with configuration values

--- lms/managers/FolioService.swift
+++ lms/managers/FolioService.swift
@@ -4,11 +4,8 @@ import SwiftUI
 
 class FolioService {
     static let shared = FolioService()
-    private let apiKey = "AIzaSyD6FrGW47bSt4sqqDRFibeUeOMWQNdREdE"
-    private let baseURL =
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-04-17"
-
-        //    private let baseURL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-pro-preview-05-06"
+    private let apiKey = APIConfig.apiKey
+    private let baseURL = APIConfig.baseURL
 
     private init() {}
     private func cleanJsonResponseString(_ rawString: String) -> String {
